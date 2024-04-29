d0be21619076e1b5cf0a940bb7175cdeed22733a
minhlong99 <93129677+bugM4ker@users.noreply.github.com>
2024-04-29 20:50:43 +0700

    feat: consuming gRpc service

--- src/Services/Basket/Basket-Api/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket-Api/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Basket_Api.Models;
 using Basket_Api.Repository;
 using BuildingBlocks.CQRS;
+using Discount_gRPC.Protos;
 
 namespace Basket_Api.Basket.StoreBasket
 {
@@ -11,14 +12,16 @@ namespace Basket_Api.Basket.StoreBasket
     {
 
     }
-    public class StoreBasketHandler(IBasketRepository repository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    public class StoreBasketHandler(IBasketRepository repository, DisCountProtoService.DisCountProtoServiceClient discountProto) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand request, CancellationToken cancellationToken)
         {
+            foreach (var item in request.shoppingCart.Items)
+            {
+                var discountResponse = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+                item.Price -= discountResponse.Amount;
+            }
             await repository.StoreBasket(request.shoppingCart, cancellationToken);
-            // Handle logic here
-            // Store in DB
-            // Update cache
             return new StoreBasketResult(request.shoppingCart.UserName);
         }
     }
